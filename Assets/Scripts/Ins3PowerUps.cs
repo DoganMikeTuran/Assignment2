077540412bb26b654508df640b932d10f7b6dd3f
Dogan Mike Turan <doganmiketuran@live.dk>
2019-09-22 23:06:59 +0200

    first commit

---
+++ Assets/Scripts/Ins3PowerUps.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ins3PowerUps : MonoBehaviour
+{
+    public GameObject PickedupEffect;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+    void Pickup()
+    {
+        Instantiate(PickedupEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
